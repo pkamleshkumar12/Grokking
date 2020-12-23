64f44639a11e8963200b97d483ca6234b10bc61d
kamlesh <pkamleshkumar12@gmail.com>
2020-12-23 20:08:32 +0800

    linear regression [pending]

---
+++ google-python-exercises/financialpython/simplelinearregression.py
@@ -0,0 +1,11 @@
+import quandl
+import pandas as pd
+import numpy as np
+
+# todo: Slope and Intercept; Python Implementation; Parameter Significance; Model Significance
+def main():
+    apple_table = quandl.get("EOD/AAPL", authtoken="ZCLG2HRqUFhBZxgfbrmj")
+
+
+if __name__ == '__main__':
+    main()
